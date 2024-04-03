878b9c827abc89f97277bbd737cd22065482d868
EliasPuddini1 <131315510+EliasPuddini1@users.noreply.github.com>
2024-04-03 13:27:36 -0300

    Incorporación de los test

--- src/main/java/com/utndds/correlativas/CorrelativasApplication.java
+++ src/main/java/com/utndds/correlativas/CorrelativasApplication.java
@@ -13,31 +13,6 @@ public class CorrelativasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CorrelativasApplication.class, args);
-
-		// Crear materias y correlativas
-		Materia algoritmos = new Materia("Algoritmos",null);
-		Materia paradigmas = new Materia("Paradigmas",List.of(algoritmos));
-		Materia diseñoSistemas = new Materia("Diseño de Sistemas",List.of(paradigmas));
-
-		// Crear inscripción con las materias a verificar
-		Inscripcion inscripcion = new Inscripcion(null, List.of(diseñoSistemas));
-
-		// Crear alumno y agregar materias hechas
-		Alumno alumno = new Alumno("juan",List.of(algoritmos,paradigmas),List.of(inscripcion));
-		inscripcion.setAlumno(alumno);
-
-
-		// Verificar si la inscripción está aprobada
-		if (inscripcion.aprobada()) {
-			System.out.println("La inscripción está aprobada");
-		} else {
-			System.out.println("La inscripción no está aprobada");
-		}
-
-		alumno.inscribir(List.of(diseñoSistemas));
-
 	}
-
-
 
 }
